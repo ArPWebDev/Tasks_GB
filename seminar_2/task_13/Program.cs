72a4c5306c8ac2e2b7624dcc488295dffb128143
«ArtemPetrov» <artempetrov112@gmail.com>
2022-08-29 17:37:06 +0400

    add 2 seminar tasks

---
+++ seminar_2/task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру (слева направо) заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 6
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");   
+long number = long.Parse(Console.ReadLine()); 
+
+if (number > 99)
+{
+    long third = number / 100 % 10;
+    Console.WriteLine($"Третья цифра {third}");
+}
+else Console.WriteLine("Третьей цифры нет");
