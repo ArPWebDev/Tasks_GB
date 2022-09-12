419be204bf088e9950ce8087a1bc686feae4493d
«ArtemPetrov» <artempetrov112@gmail.com>
2022-09-12 12:48:58 +0400

    add seminar_6 tasks

---
+++ seminar_6/task_41/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+int numPos(int count) {
+    int result = 0;
+    for (int i = 1; i <= count; i++)
+    {
+        Console.Write($"Введите {i} число: ");
+        int j = Convert.ToInt32(Console.ReadLine());
+        if (j > 0) result++;
+    }
+    return result;
+}
+
+Console.Write("Введите количество чисел");
+int countNum = Convert.ToInt32(Console.ReadLine());
+int resultNumPos = numPos(countNum);
+Console.WriteLine($"{resultNumPos} числа(ел) больше нуля");
---
+++ seminar_6/task_43/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+double[] crossPoint(double[,] array) {
+    double[] points = new double[2];
+    points[0] = (array[1, 1] - array[0, 1]) / (array[0, 0] - array[1, 0]);
+    points[1] = array[0, 0] * points[0] + array[0, 1];
+    return points;
+}
+
+
+double[,] straight = new double[2, 2];
+Console.Write("Введите значение k1");
+straight[0, 0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b1");
+straight[0, 1] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2");
+straight[1, 0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2");
+straight[1, 1] = Convert.ToInt32(Console.ReadLine());
+double[] crossPoints = new double[2];
+
+if (straight[0, 0] == straight[1, 0] & straight[0, 1] != straight[1, 1]) Console.WriteLine("Прямые паралельны");
+else if (straight[0, 0] == straight[1, 0] & straight[0, 1] == straight[1, 1]) Console.WriteLine("Прямые совпадают");
+else {
+    crossPoints = crossPoint(straight);
+    Console.WriteLine($"Точки пересечения: ({String.Join("; ", crossPoints)})");
+};
